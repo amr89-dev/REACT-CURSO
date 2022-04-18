108977c3f63f4aae906aa08f672edb904e00726f
sorrus-dev <amr89.dev@gmail.com>
2022-04-18 16:50:35 -0400

    crud con reducer

--- react-reducers/src/App.js
+++ react-reducers/src/App.js
@@ -1,11 +1,14 @@
 import Counter from "./components/Counter";
 import CounterMejorado from "./components/CounterMejorado";
+import CrudApi from "./components/CrudApi";
 import ShoppingCart from "./components/ShoppingCart";
 
 function App() {
   return (
     <div>
       <h2>UseReducer</h2>
+      <hr />
+      <CrudApi />
       <hr />
       <ShoppingCart />
       <hr />
